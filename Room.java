bc3f863f7f4d773233f7804c45c7de137291af6b
h4rdstyle <none@gmail.com>
2020-04-21 13:15:49 +0200

    07. Permitidas salidas arbitrarias.

--- Room.java
+++ Room.java
@@ -37,20 +37,7 @@ public class Room
      */
     public Room getExit(String direction){
         Room nextRoom = null;
-        if(direction.equals("north")) {
-            nextRoom = direcciones.get(direction);
-        }
-        if(direction.equals("east")) {
-            nextRoom = direcciones.get(direction);
-        }
-        if(direction.equals("south")) {
-            nextRoom = direcciones.get(direction);
-        }
-        if(direction.equals("west")) {
-            nextRoom = direcciones.get(direction);
-        }
-        if(direction.equals("southwest"))
-            nextRoom = direcciones.get(direction);
+        nextRoom = direcciones.get(direction);
         return nextRoom;
     }
     
@@ -62,22 +49,7 @@ public class Room
      * @return Una descripción de las salidas existentes.
      */
     public String getExitString(){
-        String aDevolver = "";
-        if(direcciones.containsKey("north")) {
-            aDevolver += "north ";
-        }
-        if(direcciones.containsKey("south")) {
-            aDevolver += "south ";
-        }
-        if(direcciones.containsKey("east")) {
-            aDevolver += "east ";
-        }
-        if(direcciones.containsKey("west")) {
-            aDevolver += "west ";
-        }
-        if(direcciones.containsKey("southwest")){
-            aDevolver += "southwest ";
-        }
+        String aDevolver = direcciones.keySet().toString().substring(1, direcciones.keySet().toString().length()-1).replaceAll(",", "");
         return aDevolver;
     }
     /**
@@ -87,21 +59,7 @@ public class Room
      * @param sala La sala que se encuentra en la direccion indicada
      */
     public void setExit(String direccion, Room sala){
-        if(direccion.equals("north")) {
-            direcciones.put(direccion, sala);
-        }
-        if(direccion.equals("south")) {
-            direcciones.put(direccion, sala);
-        }
-        if(direccion.equals("east")) {
-            direcciones.put(direccion, sala);
-        }
-        if(direccion.equals("west")) {
-            direcciones.put(direccion, sala);
-        }
-        if (direccion.equals("southwest")){
-            direcciones.put(direccion, sala);
-        }
+        direcciones.put(direccion, sala);
     }
 
     /**
